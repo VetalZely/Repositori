f30c580e83de70b74807a4669a2cd253f40eea4c
VIC-PC\Сереня <vetal.007.ru@gmail.com>
2018-06-18 23:52:09 +0300

    Исправить ошибку в работе алгоритма

---
+++ Rijndael доделать.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing.Imaging;
+using Diplom.Models;
+using Diploma.Models;
+using System.Text;
+using System.Security.Cryptography;
+using static Diploma.MvcApplication;
+
+namespace Diploma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
