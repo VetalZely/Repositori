9c85686607c31d5e9617c1d996dd0301de65d710
VIC-PC\Сереня <vetal.007.ru@gmail.com>
2018-06-18 23:44:23 +0300

    Вывод кирилицей

---
+++ Исправлена ошибка по выводу кирилицей.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing.Imaging;
+using Diplom.Models;
+using Diploma.Models;
+using System.Text;
+using System.Security.Cryptography;
+using static Diploma.MvcApplication;
+
+namespace Diploma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CryptoEncrypted()
+        {
+            return View();
+        }
+
+
+
+        algoritm)
+ 
+[HttpPost]
+        public ActionResult CryptoEncrypted(HttpPostedFileBase upload1, string key, string
+
+
+            else if (algoritm == "TripleDES")
+            {
+                text = TripleDes.Encrypt<TripleDESCryptoServiceProvider>(text, key);
+            }
+            else if (algoritm == "Rijndael")
+            {
+                text = TripleDes.Encrypt<RijndaelManaged>(text, key);
+            }
